64080c435b46e24dceaf1ea4b78f63f95872bba1
Steven C <stevencch99@gmail.com>
2022-01-24 23:50:25 +0800

    Sort the responsed issues, add test

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -45,21 +45,38 @@ defmodule Issues.CLI do
 
   @spec process({any, any, any}) :: any
   def process(:help) do
-    IO.puts """
+    IO.puts("""
     usage: issues <user> <project> [ count | #{@default_count} ]
-    """
+    """)
+
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
+    |> sort_into_descending_order()
+    |> last(count)
   end
 
   def decode_response({:ok, body}), do: body
 
   def decode_response({:error, error}) do
-    IO.puts "Error fetching from GitHub: #{error["message"]}"
+    IO.puts("Error fetching from GitHub: #{error["message"]}")
     System.halt(2)
+  end
+
+  @spec sort_into_descending_order(any) :: list
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 ->
+      i1["created_at"] >= i2["created_at"]
+    end)
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse
   end
 end
