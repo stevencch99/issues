d94d368ee3d4f61c566c58d15d47509732c48719
Steven C <stevencch99@gmail.com>
2022-01-26 00:33:30 +0800

    Setup escript

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Issues.MixProject do
   def project do
     [
       app: :issues,
+      escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
@@ -24,5 +25,9 @@ defmodule Issues.MixProject do
       {:httpoison, "~> 1.8"},
       {:poison, "~> 5.0"}
     ]
+  end
+
+  defp escript_config do
+    [main_module: Issues.CLI]
   end
 end
