d44d2e374dce7fae4cd4488d3ef69197545d2c69
Steven C <stevencch99@gmail.com>
2022-01-31 16:08:01 +0800

    Add logger

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,4 +1,5 @@
 defmodule Issues.GithubIssues do
+  require Logger
   @user_agent  [ {"User-agent", "Elixir dave@pragprog.com"} ]
 
   # use a module attribute to fetch the value at compile time
@@ -6,6 +7,7 @@ defmodule Issues.GithubIssues do
 
   @spec fetch(any, any) :: {:error, any} | {:ok, any}
   def fetch(user, project) do
+    Logger.info("Fetching #{user}'s project #{project}")
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -52,6 +54,8 @@ defmodule Issues.GithubIssues do
                    false | nil | true | binary | list | number | {any, any, any} | map
                }}
   def handle_response({ _, %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status code=#{status_code}")
+    Logger.debug(fn -> inspect(body) end)
     {
       status_code |> check_for_error(),
       body        |> Poison.Parser.parse!()
