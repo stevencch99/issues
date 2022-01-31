204b45c9598c5d98c31d27fea64e99154bfd3aaf
Steven C <stevencch99@gmail.com>
2022-01-31 20:34:44 +0800

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -6,6 +6,9 @@ defmodule Issues.MixProject do
       app: :issues,
       escript: escript_config(),
       version: "0.1.0",
+      # project name and source_url for ExDoc to provide live links to the source code.
+      name: "Issues",
+      source_url: "https://github.com/stevencch99/issues",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -23,7 +26,9 @@ defmodule Issues.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.8"},
-      {:poison, "~> 5.0"}
+      {:poison, "~> 5.0"},
+      {:ex_doc, "~> 0.28.0"},
+      {:earmark, "~> 1.4"}
     ]
   end
 
